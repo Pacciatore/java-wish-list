b96cbae242bc51620642febacdc68ef55859f240
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-02 15:15:10 +0100

    predisposizione main e ciclo do while

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,9 +1,33 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ArrayList<String> wishList = new ArrayList<String>();
+
+		System.out.print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.print("- Benvenuto nella tua lista dei desideri! -");
+		System.out.print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+		Scanner s = new Scanner(System.in);
+
+		String choice = "";
+
+		do {
+
+			System.out.println("\nInserire un desiderio da aggiungere alla lista: ");
+			String wishToAdd = s.nextLine();
+
+			System.out.println("Continuare ad inserire desideri? [S/N]");
+			choice = s.nextLine();
+
+		} while (choice.toLowerCase().equals("s"));
+
+		s.close();
 
 	}
 
